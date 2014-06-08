b0788497532aa852afcb0570d0e40ade14e57592
Nate Walck <n8@dropbox.com>
2014-06-08 15:02:24 -0700

    Initial Commit

---
+++ Welcome Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Welcome Assistant
+//
+//  Created by Nate Walck on 6/8/14.
+//  Copyright (c) 2014 OS X Devops. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Welcome Assistant/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Welcome Assistant
+//
+//  Created by Nate Walck on 6/8/14.
+//  Copyright (c) 2014 OS X Devops. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
