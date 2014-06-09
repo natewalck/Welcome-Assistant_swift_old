94dd837de97b42fda65953e713286494f43872b8
Nate Walck <n8@dropbox.com>
2014-06-08 19:25:32 -0700

    Made sure it activated on top of all other apps

--- Welcome Assistant/AppDelegate.swift
+++ Welcome Assistant/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
+        NSApp.activateIgnoringOtherApps(true)
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
